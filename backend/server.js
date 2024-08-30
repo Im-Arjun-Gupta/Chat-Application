b99cbae0fe69dad5e9f9025389a2a3187748fcff
Arjun Gupta <guptaarjun519@gmail.com>
2024-08-30 17:37:53 +0530

    install dotenv and create an api for fetching single chat

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const chats = require('./data/data');
 const app = express();
 
-app.listen(5000, () => {
-    console.log('server is running on port 5000');
+require('dotenv').config();
+
+app.listen( process.env.PORT , () => {
+    console.log('server is running on port', process.env.PORT);
 })
 
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-app.get('/api/chats', (req, res) => {
+app.get('/api/chat', (req, res) => {
     res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((chat) => chat._id === req.params.id);
+    res.send(singleChat);
 });
 
