bdbb2c67f30256a606d7eeee8a2a5bc5d2cdf983
Arjun Gupta <guptaarjun519@gmail.com>
2024-08-30 17:26:15 +0530

    install nodemon and create api for the chats

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const chats = require('./data/data');
 const app = express();
 
 app.listen(5000, () => {
     console.log('server is running on port 5000');
 })
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.get('/api/chats', (req, res) => {
+    res.send(chats);
+});
+
