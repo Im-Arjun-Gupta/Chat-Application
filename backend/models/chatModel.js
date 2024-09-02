78e227db4ef307e551b9e3bbf5d416412b30f5a1
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-02 18:10:03 +0530

    created the messageModel, userModel and chatModel

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -5,4 +5,26 @@ const chatModel = new mongoose.Schema({
     type: String,
     trim: true,
   },
-});
+  isGroupChat: {
+    type: Boolean,
+    default: false,
+  },
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  latestMessage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message',
+  },
+  groupAdmin: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+  }
+}, {
+    timestamps: true,
+}
+);
+
+const Chat = mongoose.model('Chat', chatModel);
+module.exports = Chat;
