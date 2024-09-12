4a7f69b27e032151427c0934286149758606e628
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-12 18:21:10 +0530

    create the userRoutes folder in the project

--- backend/server.js
+++ backend/server.js
@@ -7,33 +7,18 @@ require("dotenv").config();
 
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("server is running on port", process.env.PORT);
 });
+
+dbConnect();
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-dbConnect();
+app.use('/api/user', userRoutes);
 
-app.get("/api/chat", (req, res) => {
-  res.status(200).json({
-    message: "Chats found successfully",
-    chats: chats,
-  });
-});
 
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  if (!singleChat) {
-    res.status(404).json({
-      message: "Chat not found",
-    });
-    return;
-  }
-  res.status(200).json({
-    message: "Chat found successfully",
-    chat: singleChat,
-  });
-});
