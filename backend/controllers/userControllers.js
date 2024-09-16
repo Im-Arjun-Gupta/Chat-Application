563d421cb935cfa63168fa03e532cf4d2a45da80
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-16 14:40:45 +0530

    create authmiddleware for the authorization of users

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,61 +3,76 @@ const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
-    
-    const { name, email, password, pic } = req.body;
+  const { name, email, password, pic } = req.body;
 
-    if (!name || !email || !password) {
-        res.status(400);
-        throw new Error("Please enter all the fields");
-    }
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all the fields");
+  }
 
-    const userExists = await User.findOne({email});
+  const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        res.status(400).json({message: "User already exists"});
-        throw new Error("User already exists");
-    }
+  if (userExists) {
+    res.status(400).json({ message: "User already exists" });
+    throw new Error("User already exists");
+  }
 
-    const user = await User.create({
-        name, email, password, pic
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
     });
-
-    if (user) {
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            pic: user.pic,
-            token: generateToken(user._id),
-        });
-    } else {
-        res.status(400)
-        throw new Error("Failed to create user");
-    }
-
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
 });
 
-const authUser = asyncHandler(async(req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password} = req.body;
+  const user = await User.findOne({ email });
 
-    const user = await User.findOne({email});
-
-    if (user && (await user.matchPassword(password))){
-        res.status(200).json({
-            _id:user._id,
-            name: user.name,
-            email: user.email,
-            pic: user.pic,
-            token: generateToken(user._id)
-        })
-    } else {
-        res.status(400).json({
-            message: "Invalid Email or Password"
-        })
-        throw new Error("Invalid Email or Password")
-    }
-
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({
+      message: "Invalid Email or Password",
+    });
+    throw new Error("Invalid Email or Password");
+  }
 });
 
-module.exports = { registerUser, authUser };
+// /ap/user?search=arjun
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+    const users = await User.find(keyword).find({ _id: {$ne: req.user._id}});
+    res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, authUser } = require('../controllers/userControllers');
+const { registerUser, authUser, allUsers } = require('../controllers/userControllers');
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/').post(registerUser); // used for chaining multiple methods to the same route
+router.route('/').post(registerUser).get(protect, allUsers); // used for chaining multiple methods to the same route
 router.post('/login', authUser);
 
 module.exports = router;
