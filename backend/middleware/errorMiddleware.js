5466b812355aa5dce7d271913281ef50ff529cb0
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-16 17:15:06 +0530

    create the accesschat controller

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
-const notFound = (res, req, next) => {
+const notFound = (req, res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,24 @@
 const express = require("express");
-const chats = require("./data/data");
+const app = express();
+const PORT = process.env.PORT || 5000;
 const dbConnect = require("./config/dbConnect");
 const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes")
-const {notFound, errorHandler} = require("./middleware/errorMiddleware")
-
-const app = express();
-
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 require("dotenv").config();
 
 app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("server is running on port", process.env.PORT);
-});
-
-dbConnect();
+app.use("/api/chat", chatRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-
 app.use(notFound);
 app.use(errorHandler);
+
+app.listen(PORT, () => {
+  dbConnect();
+  console.log("server is running on port", process.env.PORT);
+});
