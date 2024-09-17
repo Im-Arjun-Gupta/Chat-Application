c41c120336a675ea2e89f4feb754330ff6a4398c
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-17 17:17:01 +0530

    create a controller for the fetchallmessages

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { sendMessage } = require("../controllers/messageControllers");
+const { sendMessage, allMessages } = require("../controllers/messageControllers");
 const router = express.Router();
 
 router.route('/').post(protect ,sendMessage);
-// router.route('/:chatId').get(protect ,fetchAllMessages);
+router.route('/:chatId').get(protect ,allMessages);
 
 module.exports = router;
