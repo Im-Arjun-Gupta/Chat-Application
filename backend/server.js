01a4823c9b3732976a5f3cdf4f1a9c16015c34a5
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-18 10:58:07 +0530

    update the socket code

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ io.on("connection", (socket) => {
     console.log("user joined room: " + room);
   });
 
-  socket.on("typing", (room) => socket.in(room).emit(typing));
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
   socket.on("new message", (newMessageRecieved) => {
@@ -53,7 +53,7 @@ io.on("connection", (socket) => {
 
     if (!chat.users) return console.log("chat.users not defined");
 
-    chat.users.array.forEach(user => {
+    chat.users.forEach(user => {
       if(user._id == newMessageRecieved.sender._id) return;
 
       socket.in(user._id).emit("message recieved", newMessageRecieved);
