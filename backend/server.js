645936751fcd99063727dc6ecdbd26984f2c7b4b
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-18 15:40:37 +0530

    changes for deploying

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,22 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/message", messageRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+// --------------------Deployment--------------------
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+// --------------------Deployment--------------------
 
 app.use(notFound);
 app.use(errorHandler);
