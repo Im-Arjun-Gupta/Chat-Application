c4f47ae722da36f0e934336ee2399d711899c959
Arjun Gupta <guptaarjun519@gmail.com>
2024-09-20 10:30:09 +0530

    update the search api

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -72,7 +72,12 @@ const allUsers = asyncHandler(async (req, res) => {
     : {};
 
     let users = await User.find(keyword).find({ _id: {$ne: req.user._id}});
-    users = users.filter((u) => u.name !== (process.env.NAME || process.env.NAME2 || process.env.NAME3));
+    const namesToFilter = [
+      process.env.NAME,
+      process.env.NAME2,
+      process.env.NAME3,
+    ].filter(Boolean);
+    users = users.filter((u) => !namesToFilter.includes(u.name));
     res.send(users);
 });
 
